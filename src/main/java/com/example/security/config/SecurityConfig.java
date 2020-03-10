73b25d9e46bdf4fcabb87986e74486a14f47394a
bucrdd <whut_bcr@163.com>
2020-03-10 23:56:36 +0800

    尝试添加并理解 spring session

--- src/main/java/com/example/security/config/SecurityConfig.java
+++ src/main/java/com/example/security/config/SecurityConfig.java
@@ -1,16 +1,9 @@
 package com.example.security.config;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -23,13 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
 
-    http.formLogin().permitAll().successHandler(new AuthenticationSuccessHandler() {
-      @Override
-      public void onAuthenticationSuccess(HttpServletRequest request,
-          HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        response.addHeader("JSESSIONID", request.getSession().getId());
-      }
-    });
+    http.formLogin().loginPage("/login").permitAll();
 
     http.logout().permitAll().deleteCookies();
 
